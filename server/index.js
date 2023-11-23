4146656b92b50e3171b039ef9133de938e7f049f
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-11-23 20:20:43 -0300

    Incorporación del server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import net from "node:net";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT ?? 2323;
+
+const serverTCP = net.createServer();
+
+serverTCP.on("connection", (socket) => {
+  socket.on("data", () => {});
+
+  socket.on("close", () => {
+    console.log("Client disconnected");
+  });
+
+  socket.on("error", () => {
+    console.log("Client error");
+  });
+
+  console.log("Client connected", new Date().toLocaleString());
+});
+
+serverTCP.listen(port, () => {
+  console.log(`Server is up on port ${port} - ${new Date().toLocaleString()}`);
+});
