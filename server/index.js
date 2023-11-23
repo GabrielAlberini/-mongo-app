63b4efa84a3145c0eee54f4ed97f223c6963a952
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-11-23 20:58:24 -0300

    Incorporación del cliente y conexión con el servidor

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ const port = process.env.PORT ?? 2323;
 const serverTCP = net.createServer();
 
 serverTCP.on("connection", (socket) => {
-  socket.on("data", () => {});
+  socket.on("data", (bufferData) => {
+    const data = JSON.parse(bufferData.toString());
+    console.log(data);
+  });
 
   socket.on("close", () => {
     console.log("Client disconnected");
